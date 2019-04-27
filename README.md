7483c7baa1e04ee7359ebd481bbed1b1494896bd
Lucas Fernandes Nogueira <lucasfernandesnog@gmail.com>
2019-04-27 20:01:41 -0300

    Initial code

---
+++ README.md
@@ -0,0 +1,13 @@
+# Auth 
+
+Requst an auth token from your API and copy it to the clipboard
+
+## Usage
+
+```bash
+$ get-token {env}
+```
+
+## API file
+
+The API file contains the environments and its API usage (url and data to POST). Each key is an **env** you can use via CLI.
---
+++ cli.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+const request = require('request')
+const clipboardy = require('clipboardy')
+
+const api = require('./api.json')
+
+const env = process.argv[2] || 'dev'
+
+const url = api[env].url
+if (!url) {
+    console.error(`Env ${env} not found. Please check your api.json`)
+    process.exit(1)
+}
+
+let postData = api[env].post
+if (!postData) {
+    console.error("Your env API object must have a 'post' property, containing the request data")
+    process.exit(1)
+}
+
+request.post(url, postData, (error, response, body) => {
+    if (error) {
+        console.error(error)
+        process.exit(1)
+    }
+
+    let data = JSON.parse(body)
+
+    let token
+
+    if (typeof data === 'string') {
+        token = data
+    } else {
+        token = data.CampoToken
+        if (token == null) {
+            console.error('Unable to parse token from response. Data:', data)
+            process.exit(1)
+        }
+    }
+
+    clipboardy.writeSync(token)
+})
